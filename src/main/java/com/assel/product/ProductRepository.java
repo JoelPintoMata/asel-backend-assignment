f8fe2ec6247a0e68e85621b769120a44efeb5f4d
joelmatacv@runbox.com <G1thub@2012>
2020-02-18 22:31:06 +0100

    Implementing product repository class

--- src/main/java/com/assel/product/ProductRepository.java
+++ src/main/java/com/assel/product/ProductRepository.java
@@ -3,9 +3,15 @@ package com.assel.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAll();
+
+    Product save(Product product);
+
     Optional<Product> findById(Long id);
 }
