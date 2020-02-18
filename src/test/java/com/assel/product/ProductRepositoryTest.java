88d92b4cd022fdc3cffff6855469d5e400a70075
joelmatacv@runbox.com <G1thub@2012>
2020-02-18 22:52:07 +0100

    Implementing product repository test class

--- src/test/java/com/assel/product/ProductRepositoryTest.java
+++ src/test/java/com/assel/product/ProductRepositoryTest.java
@@ -2,44 +2,91 @@ package com.assel.product;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @DataJpaTest
 class ProductRepositoryTest {
 
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    @Autowired
-    private EntityManager entityManager;
     @Autowired
     private ProductRepository productRepository;
 
     @Test
     void findAll() {
-        productRepository.findAll();
+        Product product = new Product();
+        product.setId(Long.valueOf(UUID.randomUUID().toString()));
+        product.setName("name");
+        product.setCurrentPrice("123.456");
+        product.setLastUpdate(Instant.now().toString());
+        productRepository.save(product);
+
+        Product otherProduct = new Product();
+        otherProduct.setId(Long.valueOf(UUID.randomUUID().toString()));
+        otherProduct.setName("other name");
+        otherProduct.setCurrentPrice("234.567");
+        otherProduct.setLastUpdate(Instant.now().toString());
+        productRepository.save(otherProduct);
+
+        List<Product> productList = productRepository.findAll();
+        Assert.assertNotNull(productList);
+        Assert.assertEquals(2, productList.size());
+
+        Assert.assertEquals(productList.get(0).getId(), product.getId());
+        Assert.assertEquals(productList.get(0).getName(), product.getName());
+        Assert.assertEquals(productList.get(0).getCurrentPrice(), product.getCurrentPrice());
+        Assert.assertEquals(productList.get(0).getLastUpdate(), product.getLastUpdate());
+
+        Assert.assertEquals(productList.get(1).getId(), otherProduct.getId());
+        Assert.assertEquals(productList.get(1).getName(), otherProduct.getName());
+        Assert.assertEquals(productList.get(1).getCurrentPrice(), otherProduct.getCurrentPrice());
+        Assert.assertEquals(productList.get(1).getLastUpdate(), otherProduct.getLastUpdate());
     }
 
     @Test
     void save() {
+        Long id = Long.valueOf(UUID.randomUUID().toString());
+
         Product product = new Product();
+        product.setId(id);
+        product.setName("name");
+        product.setCurrentPrice("123.456");
+        product.setLastUpdate(Instant.now().toString());
         productRepository.save(product);
+
+        Optional<Product> optionalOtherProduct = productRepository.findById(id);
+        Assert.assertNotNull(optionalOtherProduct);
+        Product otherProduct = optionalOtherProduct.get();
+        Assert.assertEquals(product.getId(), otherProduct.getId());
+        Assert.assertEquals(product.getName(), otherProduct.getName());
+        Assert.assertEquals(product.getCurrentPrice(), otherProduct.getCurrentPrice());
+        Assert.assertEquals(product.getLastUpdate(), otherProduct.getLastUpdate());
     }
 
     @Test
     void findById() {
         Long id = Long.valueOf(UUID.randomUUID().toString());
+
         Product product = new Product();
-        Assert.assertEquals(product, productRepository.findById(id));
+        product.setId(id);
+        product.setName("name");
+        product.setCurrentPrice("123.456");
+        product.setLastUpdate(Instant.now().toString());
+        productRepository.save(product);
+
+        Optional<Product> optionalOtherProduct = productRepository.findById(id);
+        Assert.assertNotNull(optionalOtherProduct);
+        Product otherProduct = optionalOtherProduct.get();
+        Assert.assertEquals(product.getId(), otherProduct.getId());
+        Assert.assertEquals(product.getName(), otherProduct.getName());
+        Assert.assertEquals(product.getCurrentPrice(), otherProduct.getCurrentPrice());
+        Assert.assertEquals(product.getLastUpdate(), otherProduct.getLastUpdate());
     }
 }
